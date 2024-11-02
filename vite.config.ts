da3b798a2925f27729c72efcd62c329ef37cb315
Wanling Fu <wanlingfu3@gmail.com>
2024-11-02 17:20:41 +0000

    调整Vite配置以适应GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/personal-page-frontend/',
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
