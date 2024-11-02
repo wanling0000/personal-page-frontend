08d9956c510c667fb11e61adcdddb1d2a71fa3c0
Wanling Fu <wanlingfu3@gmail.com>
2024-11-02 17:27:08 +0000

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/personal-page-frontend/',
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
