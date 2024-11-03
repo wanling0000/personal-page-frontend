3ed88a75a8892394afff0ca620731bd37362ff82
Wanling Fu <wanlingfu3@gmail.com>
2024-11-03 00:04:40 +0000

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -13,10 +13,14 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:7921', // 目标服务器地址
+        target: 'http://127.0.0.1:7921',
         changeOrigin: true, // 是否改变域名
-        rewrite: (path) => path.replace(/^\/api/, '') // 路径重写
+        secure: false, // 如果目标地址使用 https，需要设置为 false
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
+
+
+
 })
